64531e535fc81e2f503fbf2ca605bcb6e416556f
Daniyal Ahmed <da9033431@gmail.com>
2025-05-03 19:23:23 +0500

    Get request

--- src/app.js
+++ src/app.js
@@ -1,8 +1,31 @@
 const express = require("express")
 const app  = express()
 
-app.use("/",(req,res)=>{
-    res.send("hello")
+const users = [{
+    name: "John",
+    kidneys : [{
+        healthy : false
+    },{
+        healthy : true
+    }]
+}]
+
+app.get("/",(req,res)=>{
+    const johnKidneys = users[0].kidneys
+    const numberOfKidneys = johnKidneys.length
+    let numberOfHealthyKidneys = 0
+    for(let i = 0 ; i<numberOfKidneys ; i++){
+        if(johnKidneys[i].healthy === true){
+            numberOfHealthyKidneys++;
+        }
+    }
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys
+
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys
+    })
 })
 
 app.listen(3000)
